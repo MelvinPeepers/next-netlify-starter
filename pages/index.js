2522086d0038536b1985daf77d3a3062d4442e7d
Melvin Peepers <melvinpeepers@melvins-mbp.myfiosgateway.com>
2021-03-31 05:49:11 -0400

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next.js and Netlify</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Testing Next.js with Netlify" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Setting up a branch subdomain
         </p>
       </main>
 
